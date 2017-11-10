d84982bc1d7a2accc011c7d75149da57cec65790
wangchao <magician.of.technique@gmail.com>
2017-11-10 10:04:36 +0800

    [+] init annotations, compiler

---
+++ mrouter/src/main/java/im/wangchao/mrouter/Router.java
@@ -0,0 +1,11 @@
+package im.wangchao.mrouter;
+
+/**
+ * <p>Description  : Router.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2017/11/10.</p>
+ * <p>Time         : 上午9:26.</p>
+ */
+public class Router {
+
+}
